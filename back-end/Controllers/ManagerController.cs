91be0fcdedae2019f06562daa9cc82e7b4283176
Anush <an97an97@mail.ru>
2020-03-01 00:02:17 +0400

    Add files via upload

---
+++ back-end/Controllers/ManagerController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using backend.Models;
+using backend.Managers;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ManagerController : ControllerBase
+    {
+        UserManagerDB user = new UserManagerDB();
+
+        [Route("getbyid/{id}")]
+        [HttpGet]
+        public ActionResult GetById(int id)
+        {
+            try
+            {
+
+                var users = user.ReadById(id);
+
+                return Ok(users);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [Route("update")]
+        [HttpPut]
+        public ActionResult UpDateEmail(UserManager us)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var users = user.Updatet(us);
+
+                    if (users == null) return NotFound();
+                    return Ok(users);
+                }
+            }
+            catch
+            {
+                return BadRequest();
+
+
+            }
+            return BadRequest(ModelState);
+        }
+      
+    }
+}
