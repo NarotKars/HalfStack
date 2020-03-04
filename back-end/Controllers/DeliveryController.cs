0078cd5feeaecee054bd615fa352e2d94d25b026
Anush <an97an97@mail.ru>
2020-03-04 22:19:14 +0400

    Add files via upload

---
+++ back-end/Controllers/DeliveryController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using backend.Models;
+using backend.Managers;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeliveryController : ControllerBase
+    {
+        UserDeliveryDB user = new UserDeliveryDB();
+
+        [Route("getbyid/{id}")]
+        [HttpGet]
+        public ActionResult GetById(int id)
+        {
+            try
+            {
+
+                var users = user.ReadById(id);
+
+                return Ok(users);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+        [Route("update")]
+        [HttpPut]
+        public ActionResult Updatee(Update us)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var users = user.Updatet(us);
+
+                    if (users == null) return NotFound();
+                    return Ok(users);
+                }
+            }
+            catch
+            {
+                return BadRequest();
+
+
+            }
+            return BadRequest(ModelState);
+        }
+
+
+    }
+}
