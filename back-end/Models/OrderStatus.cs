1f6f83dc26cde274664580ae1ccc2cb6c83a86af
Anush <an97an97@mail.ru>
2020-03-05 16:34:19 +0400

    Update OrderStatus.cs

--- back-end/Models/OrderStatus.cs
+++ back-end/Models/OrderStatus.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace backend.Managers
+﻿namespace back_end.Models
 {
     public class OrderStatus
     {
