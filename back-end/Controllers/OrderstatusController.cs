45589028607c0264a731e006ded48c75e52c4f92
Anush <an97an97@mail.ru>
2020-03-05 16:19:29 +0400

    Add files via upload

---
+++ back-end/Controllers/OrderstatusController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using backend.Managers;
+using backend.Models;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderstatusController : ControllerBase
+    {
+        OrderStatusDB order = new OrderStatusDB();
+        [Route("update")]
+        [HttpPut]
+        public ActionResult Updatee(OrderStatus ord)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var orders= order.Update(ord);
+
+                    if (orders == null) return NotFound();
+                    return Ok(orders);
+                }
+            }
+            catch
+            {
+                return BadRequest();
+
+
+            }
+            return BadRequest(ModelState);
+        }
+
+    }
+}
